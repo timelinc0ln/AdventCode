58143d3ef14a4024160b4bc23c0d7fadb3d457cb
timelinc0ln <patrickcasey2014@gmail.com>
2020-12-01 08:42:31 -0800

    made it look prettier

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ with open('data.csv', mode='r') as csv_file:
     for row in csv_reader:
         values.append(int(row[0]))
     values.sort()
+
+    # find two numbers that sum to 2020
+    for i in range(len(values)):
+        for j in range(1,len(values)):
+            total = values[i] + values[j]
+            if total == 2020:
+                print("Values are:", values[i], values[j])
+                print("Product is:", values[i] * values[j])
+                break
+
+    # find three numbers that sum to 2020
     for i in range(len(values)):
         for j in range(1,len(values)):
             firsttwo = values[i] + values[j]
             diff = 2020 - firsttwo
             if diff in values:
-                print(values[i], values[j], diff)
-                print(values[i] * values[j] * diff)
+                print("Values are:", values[i], values[j], diff)
+                print("Product is:", values[i] * values[j] * diff)
+                break
 
 
     
